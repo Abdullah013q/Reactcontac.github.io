0589d72745e46264fd960b46b1cd4544e1be2d2b
Abdullah013q <abdullah013q@gmail.com>
2024-01-05 06:41:03 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Contactform from './components/contactform'
+
+function App() {
+
+  return (
+    <>
+      <Contactform/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/contactform.jsx
@@ -0,0 +1,60 @@
+import React, { useRef } from 'react';
+import '../App.css'
+
+ const contactform = () => {
+
+  return (
+    <form action='https://api.web3forms.com/submit' method='POST' className='form'>
+      <input type="hidden" name="access_key" value={'81ab887d-2925-42b0-9836-a684efe9f3f0'} />
+      <input type="text" name="user_name" className='name' placeholder='Your Name' autoComplete='on' required />
+      <input type="email" name="user_email" className='email' placeholder='Your Email' autoComplete='on' required />
+      <textarea name="message" className='message' placeholder='Type here for message' />
+      <input type="submit" value="Send" className='button'  />
+    </form>
+  );
+};
+
+export default contactform;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useRef } from 'react';
+// import '../App.css'
+
+//  const contactform = () => {
+
+//   return (
+//     <form action='https://formspree.io/f/xjvnvpya' method='POST' className='form'>
+//       <input type="text" name="user_name" className='name' placeholder='Your Name' autoComplete='on' required />
+//       <input type="email" name="user_email" className='email' placeholder='Your Email' autoComplete='on' required />
+//       <textarea name="message" className='message' placeholder='Type here for message' />
+//       <input type="submit" value="Send" className='button'  />
+//     </form>
+//   );
+// };
+
+// export default contactform;
