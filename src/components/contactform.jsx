6c53a16c483784435ad8cf547deff87ef566c90f
Abdullah013q <abdullah013q@gmail.com>
2024-01-06 04:28:29 +0500

    a

--- src/components/contactform.jsx
+++ src/components/contactform.jsx
@@ -6,8 +6,8 @@ import '../App.css'
   return (
     <form action='https://api.web3forms.com/submit' method='POST' className='form'>
       <input type="hidden" name="access_key" value={'81ab887d-2925-42b0-9836-a684efe9f3f0'} />
-      <input type="text" name="user_name" className='name' placeholder='Your Name' autoComplete='on' required />
-      <input type="email" name="user_email" className='email' placeholder='Your Email' autoComplete='on' required />
+      <input type="text" name="name" className='name' placeholder='Your Name' autoComplete='on' required />
+      <input type="email" name="email" className='email' placeholder='Your Email' autoComplete='on' required />
       <textarea name="message" className='message' placeholder='Type here for message' />
       <input type="submit" value="Send" className='button'  />
     </form>
